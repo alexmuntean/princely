50904ed8f50b49aff1d5f95c8986c838d10ed45e
Koen Handekyn <koen.handekyn@up-nxt.com>
2013-08-30 11:06:39 +0200

    public method asset path helper

--- lib/princely/pdf_helper.rb
+++ lib/princely/pdf_helper.rb
@@ -61,7 +61,7 @@ module PdfHelper
   def localize_html_string(html_string)
     # Make all paths relative, on disk paths...
     html_string.gsub!(".com:/",".com/") # strip out bad attachment_fu URLs
-    html_string.gsub!( /src=["']+([^:]+?)["']/i ) { |m| "src=\"#{asset_file_path($1)}\"" } # re-route absolute paths
+    html_string.gsub!( /src=["']+([^:]+?)["']/i ) { |m| "src=\"#{asset_file_path($1)}\"" } # re-route absolute paths    
 
     # Remove asset ids on images with a regex
     html_string.gsub!( /src=["'](\S+\?\d*)["']/i ) { |m| 'src="' + $1.split('?').first + '"' }
